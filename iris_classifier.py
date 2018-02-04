dc75103199e38a748a8a47d47448e49b87ab33bb
Rafael Amadigi Dal Santo <rafaeldalsanto@gmail.com>
2018-02-04 14:42:01 -0200

    iris

---
+++ iris_classifier.py
@@ -0,0 +1,19 @@
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+from sklearn import tree
+classifier = tree.DecisionTreeClassifier()
+
+classifier.fit(X_train, y_train)
+
+predictions = classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predictions))
