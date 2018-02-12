5e6392abba8b962310add88142f93543f5c8143f
Rafael Amadigi Dal Santo <rafaeldalsanto@gmail.com>
2018-02-12 17:09:47 -0200

    Multiple linear regression.

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -2,9 +2,13 @@
 
 # Importing the libraries
 import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import warnings
+
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
@@ -29,3 +33,10 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+print('Coefficients: \n', regressor.coef_)
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % r2_score(y_test, y_pred))
+
+print('y_test: \n', y_test)
+print('y_pred: \n', y_pred)
